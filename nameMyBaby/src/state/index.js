4ea1790c3f90174f38c2a59bf8722cad7c1510dd
victoriajquinto <victoriajquinto@gmail.com>
2023-09-18 21:36:13 -0700

    remove favorites state in redux

--- nameMyBaby/src/state/index.js
+++ nameMyBaby/src/state/index.js
@@ -2,12 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import NameReducer from './name/NameReducer.js';
 import InfoReducer from './Info/infoReducer.js';
-import FavReducer from './Favorites/favReducer.js'
 
 const rootReducer = combineReducers({
   name: NameReducer,
   info: InfoReducer,
-  fav: FavReducer
 });
 
 export const store = createStore(
