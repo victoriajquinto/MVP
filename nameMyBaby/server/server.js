0679e756076239598da7e6943db02635a507c229
victoriajquinto <victoriajquinto@gmail.com>
2023-09-20 13:04:10 -0700

    revert back to development mode

--- nameMyBaby/server/server.js
+++ nameMyBaby/server/server.js
@@ -7,6 +7,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+
 const app = express();
 
 app.use(express.json());
--- nameMyBaby/vite.config.js
+++ nameMyBaby/vite.config.js
@@ -1,4 +1,4 @@
-git import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
