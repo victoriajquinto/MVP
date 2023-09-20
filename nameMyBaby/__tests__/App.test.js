6726c69bfcfb0c50fdebc2f8adb91cb2984fc778
victoriajquinto <victoriajquinto@gmail.com>
2023-09-20 10:42:25 -0700

    remove tests

--- nameMyBaby/__tests__/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react"
-import App from "../src/components/App.jsx";
-
-test("Renders the main page", () => {
-  render(<App />)
-  expect(true).toBeTruthy()
-})
